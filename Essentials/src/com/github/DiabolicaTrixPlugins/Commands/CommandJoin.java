a97dded6b451b0190c92da29f1938b00395a57b5
DiabolicaTrixPlugins <macmocus7@gmail.com>
2013-11-26 19:03:44 -0500

    Test

--- Essentials/src/com/github/DiabolicaTrixPlugins/Commands/CommandJoin.java
+++ Essentials/src/com/github/DiabolicaTrixPlugins/Commands/CommandJoin.java
@@ -1,13 +1,9 @@
 package com.github.DiabolicaTrixPlugins.Commands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import com.github.DiabolicaTrixPlugins.Essentials;
 
